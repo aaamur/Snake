a438c06ff18e7cd35f1151134dc0d77d1c596f35
Sasha Abdulova <sanchoponchos@gmail.com>
2017-01-21 12:51:21 +0600

    Lesson 3 Class and Object

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 4;
+            p1.y = 5;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 10;
+            p2.y = 11;
+            p2.sym = '#';
+            p2.Draw();
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
