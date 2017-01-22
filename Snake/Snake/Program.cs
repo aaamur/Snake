ef1a5a8bc760c5c6da02add1400392cff314eda2
Sasha Abdulova <sanchoponchos@gmail.com>
2017-01-22 10:04:16 +0600

    Lesson 6 List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,8 +16,27 @@ namespace Snake
             Point p2 = new Point(10,12,'#');
             p2.Draw();
 
-            Console.ReadLine();
+            Point p3 = new Point(8, 20, '$');
+            p2.Draw();
 
+            Point p4 = new Point(19, 30, '&');
+            p2.Draw();
+
+            List<Point> pList = new List<Point> ();
+
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            Console.WriteLine();
+            foreach (Point i in pList)
+            {
+                Console.WriteLine(i.x);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
